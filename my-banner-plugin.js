5139f6e58f62496a1bbed9391c4b3669fe223da2
delaylee <delaylee219@gmail.com>
2022-08-29 22:41:50 +0900

    :zap: update BannerPlugin
    - childProcess를 사용하여 git commit id, uesr.name을 가져오도록 추가

--- my-banner-plugin.js
+++ my-banner-plugin.js
@@ -1,8 +1,12 @@
+const childProcess = require('child_process');
+
 function banner() {
   const data = new Date().toLocaleString();
   const banner = [
     '이것은 BannerPlugin이 처리한 결과입니다.',
     `Build Data: ${data}`,
+    `Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}`,
+    `Author: ${childProcess.execSync('git config user.name')}`
   ].join('\n');
 
   return banner;
