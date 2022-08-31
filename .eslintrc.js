1acaa96b153ed64d26d9436bc2053a8a0e7d3ce2
delaylee <delaylee219@gmail.com>
2022-08-31 22:55:15 +0900

    :sparkles: add: eslint-plugin-prettier
    plugin을 이용하여 eslint로 prettier까지 통합하여 동작하도록 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
