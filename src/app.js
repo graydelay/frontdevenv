4b770d373eef710a461c49a81565f108f5af3377
delaylee <delaylee219@gmail.com>
2022-09-03 23:05:23 +0900

    :sparkles: add: hot module replacement
    hot module을 사용하여 변경된 모듈만 바꿔치기 하도록 만든다

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
-import axios from 'axios';
+import form from './form';
+import result from './result';
+import './app.css'
+
+let resultEl;
+let formEl;
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const res = await axios.get('/api/users');
+  formEl = document.createElement('div');
+  formEl.innerHTML = form.render();
+  document.body.appendChild(formEl);
 
-  document.body.innerHTML = (res.data || []).map(user => {
-    return `<div>${user.id}: ${user.name}</div>`;
-  }).join('');
-});
+  resultEl = document.createElement('div');
+  resultEl.innerHTML = await result.render();
+  document.body.appendChild(resultEl);
+})
+
+if (module.hot) {
+  console.log('핫 모듈 켜짐');
+
+  module.hot.accept('./result', async () => {
+    console.log('reulst 모듈 변경됨');
+
+    resultEl.innerHTML = await result.render();
+  })
+
+  module.hot.accept('./form', () => {
+    console.log('reulst 모듈 변경됨');
+
+    formEl.innerHTML = form.render();
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
 
       return middlewares;
     },
+    hot: true,
   },
   module: {
     rules: [
